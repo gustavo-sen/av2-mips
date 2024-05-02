de05f652e74ed8b6e862d28588d67623a68671a6
gustavo-sen <gustavoabdon2@gmail.com>
2024-05-02 11:29:16 -0300

    init code

---
+++ mips_av2.asm
@@ -0,0 +1,144 @@
+.eqv index_lb $t0
+.eqv multiplicador $t1
+.eqv index_sw $t2
+.eqv index $t3
+.eqv char $s0
+.eqv digit $s0
+.eqv num $s2
+
+#========================================#
+.data
+    tamanho: .word 100
+    concatenarLinha: .asciiz ", "
+    unsorted_list_name: .asciiz "unsortedList.txt"
+    sorted_list_name: .asciiz "sorted_list.txt"
+
+    unsorted_list: .space 400
+    .align 2
+    lista_numerica: .space 400
+.text
+
+li index_sw, 0
+li index_lb, 0
+li index, 0
+#========================================#
+
+main:
+    jal readFile                 # function to load file into RAM
+    la $s0, lista_numerica      # load adress of lista_numerica
+    lw $s1, tamanho             # store size of list
+    jal bubbleSort              # performe sort algorithm
+    li $t0, 0                   # reset $t0 (index_lb) 
+    jal writeFile
+    j exit
+
+#========================================#
+init:
+    li num, 0
+    li multiplicador, 1
+    jal store
+
+loop:
+    lb char, unsorted_list(index_lb)
+    add index_lb, index_lb, 1
+    beq char, '\0', store
+    beq char ',', store
+    beq char,'-', sinal
+    sub digit, char, 0x30
+    mul num, num 10
+    add num, num, digit
+    j loop
+    sinal:
+    li multiplicador, -1
+    j loop
+
+store:
+    mul num, num, multiplicador
+    sw num, lista_numerica(index_sw)
+    add index_sw, index_sw, 4
+    beq char, '\0', writeFile
+    j init
+
+bubbleSort:
+    li $t2, 1
+
+outerLoop:
+    li $t2, 0
+    li $t3, 0
+
+innerLoop:
+    mul $t4, $t3, 4
+    add $t5, $s0, $t4
+    addi $t6, $t5, 4
+
+    lw $t7, 0($t5)
+    lw $t8, 0($t6)
+
+    ble $t7, $t8, pula
+    sw $t8, 0($t5)
+    sw $t7, 0($t6)
+    li $t2, 1
+
+pula:
+    addi $t3, $t3, 1
+    blt $t3, $s1, innerLoop
+    beqz $t2, acabarSort
+    jal outerLoop
+
+acabarSort:
+    jr $ra
+    j writeFile
+
+#========================================#
+readFile:
+
+    li $v0, 13                  # open file
+    la $a0, unsorted_list_name  # file path
+    li $a1, 0                   # read command 0
+    syscall     
+
+    move $s0, $v0               # copy descriptor
+    move $a0, $s0               # save value in $a0
+
+    li $v0, 14                  # read data pointed by $a0
+    la $a1, lista_numerica      # buffer to store data
+    li $a2, 400                 # buffer size
+    syscall 
+    
+    li $v0, 4                   
+    move $a0, $a1
+    syscall
+
+    # close file
+    li $v0, 16
+    move $a0, $s0
+    syscall
+
+#========================================#
+
+writeFile:
+    # Open (for writing) a file that does not exist
+    li   $v0, 13                    # system call for open file
+    la   $a0, sorted_list_name      # output file name
+    li   $a1, 1                     # Open for writing (flags are 0: read, 1: write)
+    li   $a2, 0                     # mode is ignored
+    syscall                         # open a file (file descriptor returned in $v0)
+    move $s6, $v0                   # save the file descriptor 
+
+    # Write to file just opened
+    li   $v0, 15                    # system call for write to file
+    move $a0, $s6                   # file descriptor 
+    la   $a1, lista_numerica        # address of buffer from which to write
+    li $a2, 401                     # hardcoded buffer length
+    syscall                         # write to file
+   
+    # Close the file 
+    li   $v0, 16                    # system call for close file
+    move $a0, $s6                   # file descriptor to close
+    syscall                         # close file
+
+#========================================#
+# End program
+exit:
+    li $v0, 10                      # Exit command
+    syscall         
